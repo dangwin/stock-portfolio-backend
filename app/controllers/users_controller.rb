4998906d336e9cd85fd9416d8a101f5d386dc209
dangwin <danghwin@gmail.com>
2021-06-15 20:18:38 -0500

    backend ready

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,12 @@ class UsersController < ApplicationController
     end
 
     def show
-        @user = @user.find(params[:id])
+        @user = User.find(params[:id])
+        render json: @user
     end
 
     def create
-        @user = @user.stocks.build(stock_params)
+        @user = User.new(user_params)
 
         if @user.save
             render json: @user
@@ -18,15 +19,30 @@ class UsersController < ApplicationController
         end
     end
 
-    def destroy
-        @user = @user.find(params[:id])
+    def update
+        @user = User.find(params[:id])
 
-        if @suser && @user.destroy
+        if @user && @user.update(user_params)
+            render json: @user
+        else
+            head status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        @user = User.find(params[:id])
+
+        if @user && @user.destroy
             head status: :success
         else
             head status: :not_found
         end
     end
 
+    private
+
+    def user_params
+        params.require(:user).permit(:name)
+    end
 
 end
